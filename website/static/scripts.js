a70e78e7401e9380148f99d94a3b1ec149c31e69
ChegeMicheal <121975024+ChegeMicheal@users.noreply.github.com>
2024-08-17 11:36:09 +0300

    Update scripts.js

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -71,11 +71,11 @@ $(".toggleIcon").click(function(){
    if($(this).attr("data-permission-value")=="true")
 {
     $(this)
-        .attr("data-permission-value", "true")
+        .attr("data-permission-value", "false")
         .addClass("navyIcon"); 
 }else{
     $(this)
-        .attr("data-permission-value", "false")
+        .attr("data-permission-value", "true")
         .removeClass("navyIcon");
 }
 
